94885776f1a03dc169efd26ee924ace99fe01f68
ncevallos27 <cevallos@wustl.edu>
2025-11-20 12:41:12 -0600

    new build patterns still work

--- testing.py
+++ testing.py
@@ -1,6 +1,13 @@
+import finProj as fp
 def main():
     """Main entry point of the program."""
-    pass
+    p = fp.BinomialTree(1, 1/8.0, 0.04)
+    s = fp.Stock(p, 52, 0.28)
+    pf = fp.EuroCall()
+    o = fp.Option(s, pf, 50, 0.5, fp.OptionPosition.Long)
+
+    print(o.price())
+
 
 
 if __name__ == "__main__":
